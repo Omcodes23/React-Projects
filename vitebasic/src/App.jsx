e186296a289318349dcaed78aacb6f6550599410
Om vataliya <109670967+Omcodes23@users.noreply.github.com>
2023-10-27 23:52:30 +0530

    Vite Basic App

---
+++ vitebasic/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>
+    <Chai/>
+    <h1>this is app.jsx file</h1>
+    <h3>This is the h3 tag</h3>
+    </>
+  )
+}
+
+export default App
---
+++ vitebasic/src/chai.jsx
@@ -0,0 +1,6 @@
+function chai(){
+    return(
+        <h2>CHai is rady</h2>
+    )
+}
+export default chai;
